39616fd893717ad0b696e5e14c5d1c9af7edb860
SvetlanaShipovalova <90263099+SvetlanaShipovalova@users.noreply.github.com>
2024-03-16 12:09:52 +0300

    update

--- flaskProject/main/__init__.py
+++ flaskProject/main/__init__.py
@@ -1,9 +1,1 @@
-from flask import Flask
-from flask_bootstrap import Bootstrap5
-
-myApp = Flask(__name__)
-myApp.config["SECRET_KEY"] = "hard to unlock"
-bootstrap = Bootstrap5(myApp)
-
-from main import forms
-from main import routes
+from app import myApp
